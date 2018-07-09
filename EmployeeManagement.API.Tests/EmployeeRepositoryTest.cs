ee8688fd54587fa3dfb1a0edafa0fbad695d2b1a
Ivan Sukhoi <ivan.hiqo-solutions.com>
2018-07-09 19:26:43 +0300

    Update tests

--- EmployeeManagement.API.Tests/EmployeeRepositoryTest.cs
+++ EmployeeManagement.API.Tests/EmployeeRepositoryTest.cs
@@ -5,13 +5,13 @@ namespace EmployeeManagement.API.Tests
 {
     public class EmployeeRepositoryTest
     {
-        private EmployeeRepository _employeeRepository;
-        private IWebClient _webClient;
+        //private EmployeeRepository _employeeRepository;
+        //private IWebClient _webClient;
 
-        public void SetUp()
-        {
-            _employeeRepository = new EmployeeRepository();
+        //public void SetUp()
+        //{
+        //    _employeeRepository = new EmployeeRepository();
 
-        }
+        //}
     }
 }
